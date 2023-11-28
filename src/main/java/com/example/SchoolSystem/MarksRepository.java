2f5f847176b3476724df32ee9d5ad042e526acfd
LENOVO <ishimwelauraceline@gmail.com>
2023-11-28 00:32:18 -0800

    some changes

---
+++ src/main/java/com/example/SchoolSystem/MarksRepository.java
@@ -0,0 +1,8 @@
+package com.example.SchoolSystem;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface MarksRepository extends JpaRepository<Marks, UUID> {
+}
